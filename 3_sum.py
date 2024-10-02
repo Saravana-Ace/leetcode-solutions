3470d0e2ced6ea7b1d9011c71db3582254ad1aad
Saravana-Ace <saravanaking1@gmail.com>
2024-10-01 20:04:45 -0400

    updated 3sum from nums[k] to val

--- 3_sum.py
+++ 3_sum.py
@@ -1,23 +1,23 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        nums = sorted(nums)
-        res = set()
+        nums.sort()
+        res = []
 
         for k, val in enumerate(nums):
             if k > 0 and val == nums[k-1]:
                 continue
             if val > 0:
-                break
+                return res
             i, j = k+1, len(nums)-1
             
             while i < j:
-                total = nums[k] + nums[i] + nums[j]
+                total = val + nums[i] + nums[j]
                 if total < 0:
                     i += 1
                 elif total > 0:
                     j -= 1
                 else:
-                    res.add(tuple(sorted((nums[k], nums[i], nums[j]))))
+                    res.append((val, nums[i], nums[j]))
                     i+=1
                     while nums[i] == nums[i-1] and i < j:
                         i += 1
