53411bda54dd808de4e9baeb9c680e96be247b2c
Saravana Polisetti <saravanapolisetti@Saravanas-MacBook-Pro.local>
2025-01-17 16:40:35 -0500

    better solution for palindromic substrings problem using inward out palindrome check

--- palindromic_substrings.py
+++ palindromic_substrings.py
@@ -31,3 +31,21 @@ class Solution:
             j = hold
         
         return res
+
+#solution 2 - finding palindromes from inward out to reduce redundancy
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        res = 0
+        for i in range(len(s)):
+            res += self.counter(i, i, s)
+            res += self.counter(i, i+1, s)
+
+        return res
+        
+    def counter(self, l, r, s):
+        res = 0
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            res += 1
+            l -= 1
+            r += 1
+        return res
