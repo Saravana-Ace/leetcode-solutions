764ac694d9aaf45a3e3a4c8a2c20379faaceb483
Saravana Polisetti <saravanapolisetti@Saravanas-MacBook-Pro.local>
2025-01-16 23:56:14 -0500

    second attempt solution for max subarray

--- max_subarray.py
+++ max_subarray.py
@@ -10,3 +10,22 @@ class Solution:
                 curr_sum += nums[i]
             res = max(res, curr_sum)
         return res
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        curr_sum = nums[0]
+        total_sum = nums[0]
+        i = 0
+        for num in nums:
+            if i == 0:
+                i+=1
+                continue
+                
+            if num > curr_sum and curr_sum < 0:
+                curr_sum = num
+            else:
+                curr_sum += num
+
+            total_sum = max(total_sum, curr_sum)
+        
+        return total_sum
