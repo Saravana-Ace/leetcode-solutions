4e7f85f763c450833b06e5c080dc05b53c364bda
Saravana Polisetti <saravanapolisetti@Saravanas-MacBook-Pro.local>
2025-01-20 16:48:53 -0500

    merge sorted lists simple solution

--- merge_sorted_lists2.py
+++ merge_sorted_lists2.py
@@ -24,3 +24,27 @@ class Solution:
             res.next = list2
 
         return ret.next
+
+# solution 3 - similar to solution 2 but everything is inside the loop
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        res = temp = ListNode()
+        
+        while True:
+            if not list1:
+                temp.next = list2
+                break
+            elif not list2:
+                temp.next = list1
+                break
+            
+            if list1.val >= list2.val:
+                temp.next = list2
+                list2 = list2.next
+            else:
+                temp.next = list1
+                list1 = list1.next
+            
+            temp = temp.next
+        
+        return res.next
