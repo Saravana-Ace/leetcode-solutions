4840fe3eea4ec3f5a3c8cccc8d182554b1323236
Saravana-Ace <saravana.polisetti@gmail.com>
2025-01-21 17:53:56 -0500

    small change, created check variable to remove redundant calculations

--- sliding_window_maximum.py
+++ sliding_window_maximum.py
@@ -3,16 +3,18 @@ class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
         queue = deque()
         res = []
+        check = k-1
+        
         for i in range(len(nums)):
             while queue and nums[i] > nums[queue[-1]]:
                 queue.pop()
             
-            if queue and i - queue[0] + 1 > k:
+            if queue and i - queue[0] > check:
                 queue.popleft()
 
             queue.append(i)
 
-            if queue and i+1 == k or i+1 > k:
+            if queue and i+1 >= k:
                 res.append(nums[queue[0]])
         
         return res
