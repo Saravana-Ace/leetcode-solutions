ca15d812c26101e1766e6aa1c7618253a40da3cc
Saravana-Ace <saravana.polisetti@gmail.com>
2025-01-21 01:58:16 -0500

    valid palindrome solution 4

--- valid_palindrome.py
+++ valid_palindrome.py
@@ -32,3 +32,20 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         s = ''.join(filter(str.isalnum, s)).lower()
         return s == s[::-1]
+    
+# solution 4 - similar to solution 1
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        i = 0
+        j = len(s)-1
+        while i <= j:
+            while i < j and not s[i].isalnum():
+                i += 1
+            while j > i and not s[j].isalnum():
+                j -= 1
+            # if s[i].isalpha() and s[j].isalpha():
+            if s[i].lower() != s[j].lower():
+                return False
+            i += 1
+            j -= 1
+        return True
