8ee2a5d2314254e7ac4f70e35166c5fd9722a4a8
Saravana Polisetti <saravanapolisetti@Saravanas-MacBook-Pro.local>
2025-01-21 01:32:54 -0500

    valid palindrome solution 2 and 3

--- removenth_node_from_end_of_list.py
+++ removenth_node_from_end_of_list.py
@@ -23,3 +23,22 @@ class Solution:
         temp.next = temp.next.next
 
         return res.next
+    
+# solution 2 - using two pointer method
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+        left = dummy
+        right = head
+        
+        while n > 0:
+            right = right.next
+            n -= 1
+        
+        while right:
+            left = left.next
+            right = right.next
+        
+        left.next = left.next.next
+
+        return dummy.next
--- valid_palindrome.py
+++ valid_palindrome.py
@@ -16,3 +16,19 @@ class Solution:
             j -= 1
         
         return True
+
+# solution 2 - converting string to alphabets/numbers only
+class Solution:
+    def isPalindrome(self, s:str) -> bool:
+        res = ""
+        for c in s:
+            if c.isalnum():
+                res += c.lower()
+            res += c.lower() if c.isalnum() else ''
+        return res == res[::-1]
+    
+# solution 3 - using filter function, returns an iterable
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = ''.join(filter(str.isalnum, s)).lower()
+        return s == s[::-1]
