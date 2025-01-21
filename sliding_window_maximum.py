3965ee0d725947a466084694bf353bc0f96766cb
Saravana-Ace <saravana.polisetti@gmail.com>
2025-01-21 18:01:19 -0500

    second sliding window maximum solution

--- sliding_window_maximum.py
+++ sliding_window_maximum.py
@@ -18,3 +18,26 @@ class Solution:
                 res.append(nums[queue[0]])
         
         return res
+
+# solution 2 - using neetcode sliding window
+from collections import deque
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        l = r = 0
+        res = []
+        queue = deque()
+        while r < len(nums):
+            while queue and nums[r] > nums[queue[-1]]:
+                queue.pop()
+            
+            queue.append(r)
+
+            if l > queue[0]:
+                queue.popleft()
+            
+            if (r+1) >= k:
+                res.append(nums[queue[0]])
+                l += 1
+            r += 1
+        
+        return res
