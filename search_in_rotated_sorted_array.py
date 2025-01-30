8385fc21554418c1abb0dc27a793e20b099893e9
Saravana-Ace <saravana.polisetti@gmail.com>
2025-01-30 04:08:39 -0500

    search in rotated sorted array - removed unnecessary comments

--- search_in_rotated_sorted_array.py
+++ search_in_rotated_sorted_array.py
@@ -1,26 +1,25 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
+        # we only want to do binary search on a 
+        # sorted portion, so we need to first check
+        # for a valid portion of the array
+        
         l, r = 0, len(nums)-1
         while l <= r:
             mid = (l+r)//2
 
             if nums[mid] == target:
                 return mid
-            # in left portion of array
+            # left sorted portion of array
             if nums[l] <= nums[mid]:
                 if target >= nums[l] and target < nums[mid]:
                     r = mid -1
-                # if target > nums[mid] or target < nums[l]:
-                #     l = mid + 1
                 else:
                     l = mid + 1
-            # in right portion of array
+            # right sorted portion of array
             else:
                 if target > nums[mid] and target <= nums[r]:
                     l = mid + 1
-                # if target < nums[mid] or target > nums[r]:
-                #     r = mid - 1
                 else:
                     r = mid - 1
-
         return -1
