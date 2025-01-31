152717a4a514b55a216fa44e59ad8e2f89e481ba
Saravana-Ace <saravana.polisetti@gmail.com>
2025-01-30 19:45:45 -0500

    solution 2 - median of two sorted arrays

--- median_of_two_sorted_arrays.py
+++ median_of_two_sorted_arrays.py
@@ -6,3 +6,34 @@ class Solution:
             return (nums1[(len(nums1)-1)//2] + nums1[(len(nums1)-1)//2 + 1])/2
         else:
             return nums1[(len(nums1)-1)//2]
+
+# solution 2 - using binary search to find first half of sorted array
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        A, B = nums1, nums2
+        if len(B) < len(A):
+            A, B = B, A
+        
+        l, r = 0, len(A)-1
+        total = len(A) + len(B)
+        half_len = total//2
+
+        while True:
+            mid = (l+r)//2
+
+            A_left = A[mid] if mid >= 0 else float("-inf")
+            A_right = A[mid + 1] if mid + 1 < len(A) else float("inf")
+            
+            mid2 = half_len-mid-2
+            B_left = B[mid2] if mid2 >= 0 else float("-inf")
+            B_right = B[mid2+1] if mid2 + 1 < len(B) else float("inf")
+
+            if A_left <= B_right and B_left <= A_right:
+                if total % 2:
+                    return min(A_right, B_right)
+                else:
+                    return (max(A_left, B_left) + min(A_right, B_right)) / 2
+            elif A_left > B_right:
+                r = mid-1
+            else:
+                l = mid+1
