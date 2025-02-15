653c1b41d86ad11433b09ec075203e97004f79d3
Saravana-Ace <saravana.polisetti@gmail.com>
2025-02-15 15:55:19 -0500

    added comments for clarification

--- minimum_operations_to_exceed_threshold_value_2.py
+++ minimum_operations_to_exceed_threshold_value_2.py
@@ -3,7 +3,9 @@ class Solution:
     def minOperations(self, nums: List[int], k: int) -> int:
         heapq.heapify(nums)
         res = 0
-        
+
+        # since its a heap the first and second values are 
+        # in order so no need to min or max
         while nums[0] < k:
             op = heapq.heappop(nums) * 2 + heapq.heappop(nums)
             heapq.heappush(nums, op)
