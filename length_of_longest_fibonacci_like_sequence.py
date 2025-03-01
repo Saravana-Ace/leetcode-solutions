5a4a7e5308fc971a8b11f6e89469fe40ba65719e
Saravana-Ace <saravana.polisetti@gmail.com>
2025-02-28 20:57:01 -0500

    873. Length of Longest Fibonacci Subsequence - dp solution

--- length_of_longest_fibonacci_like_sequence.py
+++ length_of_longest_fibonacci_like_sequence.py
@@ -17,3 +17,25 @@ class Solution:
                     res = max(res, temp)
 
         return res
+    
+# solution 2: dp
+class Solution:
+    def lenLongestFibSubseq(self, arr: List[int]) -> int:
+        check = set(arr)
+        dp = [[0] * len(arr) for _ in range(len(arr))]
+        res = 0
+        mapper = {a:i for i, a in enumerate(arr)}
+
+        for i in reversed(range(len(arr)-1)):
+            for j in reversed(range(i+1, len(arr))):
+                prev, curr = arr[i], arr[j]
+                nxt = prev+curr
+                
+                dp[i][j] = 2
+
+                if nxt in check:
+                    temp = 1 + dp[j][mapper[nxt]]
+                    dp[i][j] = temp
+                    res = max(res, temp)
+
+        return res
