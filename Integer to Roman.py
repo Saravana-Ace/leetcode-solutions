06c026c764943fe1f8e713a44c23277e808b7771
Saravana-Ace <saravana.polisetti@gmail.com>
2025-06-02 18:27:54 -0700

    Integer to Roman

--- Integer to Roman.py
+++ Integer to Roman.py
@@ -1,21 +1,20 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        # Define value-symbol pairs in descending order
-        # Include subtractive cases (4, 9, 40, 90, 400, 900)
-        values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
-        symbols = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+        # Mapping of integers to roman numerals (descending order)
+        mapping = [
+            (1000, "M"), (900, "CM"), (500, "D"), (400, "CD"),
+            (100, "C"), (90, "XC"), (50, "L"), (40, "XL"),
+            (10, "X"), (9, "IX"), (5, "V"), (4, "IV"), (1, "I")
+        ]
         
         result = ""
         
-        # Iterate through each value-symbol pair
-        for i in range(len(values)):
-            # Calculate how many times current value fits into num
-            count = num // values[i]
-            
-            # Add the corresponding symbols to result
-            result += symbols[i] * count
-            
-            # Reduce num by the value we just processed
-            num %= values[i]
+        for value, symbol in mapping:
+            # How many times does this value fit?
+            count = num // value
+            # Add the symbols
+            result += symbol * count
+            # Update remaining number
+            num %= value
         
         return result
