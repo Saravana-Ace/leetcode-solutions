0e58d12f4f83d219acd48491aea080083cc6a503
Saravana-Ace <saravana.polisetti@gmail.com>
2025-06-09 17:19:30 -0700

    Surrounded Regions

--- Surrounded Regions.py
+++ Surrounded Regions.py
@@ -6,25 +6,37 @@ class Solution:
         Do not return anything, modify board in-place instead.
         """
         directions = [(1,0), (-1,0), (0,1), (0,-1)]
-
-        def get_island(coord):
-            queue = deque([coord])
-            visited = {coord}
-
-            while queue:
-                x,y = queue.popleft()
-
-                board[x][y] = "T"
-                for new_x, new_y in directions:
-                    new_x, new_y = new_x + x, new_y + y
-
-                    if 0 <= new_x < len(board) and 0 <= new_y < len(board[0]):
-                        if (new_x,new_y) not in visited and board[new_x][new_y]=='O':
-                            queue.append((new_x, new_y))
-                            visited.add((new_x, new_y))
-
-        
         rows, cols = len(board), len(board[0])
+
+        # def get_island(coord):
+        #     queue = deque([coord])
+        #     visited = {coord}
+
+        #     while queue:
+        #         x,y = queue.popleft()
+
+        #         board[x][y] = "T"
+        #         for new_x, new_y in directions:
+        #             new_x, new_y = new_x + x, new_y + y
+
+        #             if 0 <= new_x < len(board) and 0 <= new_y < len(board[0]):
+        #                 if (new_x,new_y) not in visited and board[new_x][new_y]=='O':
+        #                     queue.append((new_x, new_y))
+        #                     visited.add((new_x, new_y))
+        
+        visited = set()
+        def get_island(coord):
+            x,y = coord
+
+            if x < 0 or x >= rows or y < 0 or y >= cols or board[x][y] != "O" or (x,y) in visited:
+                return
+            
+            board[x][y] = "T"
+            visited.add(coord)
+            
+
+            for new_x, new_y in directions:
+                get_island((x+new_x,y+new_y))
 
         for r in range(rows):
             if board[r][0] == "O": 
