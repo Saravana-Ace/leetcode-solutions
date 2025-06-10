7772a4565c25b5c41646d34e544a457d32bdc576
Saravana-Ace <saravana.polisetti@gmail.com>
2025-06-09 17:10:29 -0700

    Surrounded Regions

--- Surrounded Regions.py
+++ Surrounded Regions.py
@@ -1,3 +1,4 @@
+# BFS Solution right now
 from collections import deque
 class Solution:
     def solve(self, board: List[List[str]]) -> None:
@@ -7,38 +8,31 @@ class Solution:
         directions = [(1,0), (-1,0), (0,1), (0,-1)]
 
         def get_island(coord):
-            queue = deque()
-            queue.append(coord)
-            visited = set()
-            visited.add(coord)
+            queue = deque([coord])
+            visited = {coord}
 
             while queue:
                 x,y = queue.popleft()
 
-                # if board[x][y] == "O":
                 board[x][y] = "T"
                 for new_x, new_y in directions:
-                    new_x += x
-                    new_y += y
+                    new_x, new_y = new_x + x, new_y + y
 
                     if 0 <= new_x < len(board) and 0 <= new_y < len(board[0]):
                         if (new_x,new_y) not in visited and board[new_x][new_y]=='O':
                             queue.append((new_x, new_y))
                             visited.add((new_x, new_y))
+
         
         rows, cols = len(board), len(board[0])
 
         for r in range(rows):
-            if board[r][0] == "O":
-                get_island((r,0))
-            if board[r][cols-1] == "O":
-                get_island((r,cols-1))
+            if board[r][0] == "O": get_island((r,0))
+            if board[r][cols-1] == "O": get_island((r,cols-1))
         
         for c in range(cols):
-            if board[0][c] == "O":
-                get_island((0,c))
-            if board[rows-1][c] == "O":
-                get_island((rows-1,c))
+            if board[0][c] == "O": get_island((0,c))
+            if board[rows-1][c] == "O": get_island((rows-1,c))
 
             
         for i in range(rows):
