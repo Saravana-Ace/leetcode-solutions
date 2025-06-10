d7979b1467906981455f85ad359e6de04ce495c3
Saravana-Ace <saravana.polisetti@gmail.com>
2025-06-10 03:38:42 -0700

    Valid Sudoku

--- Valid Sudoku.py
+++ Valid Sudoku.py
@@ -1,22 +1,20 @@
 from collections import defaultdict
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        number_locations = defaultdict(set)
+        squares = defaultdict(set)
         number_row = defaultdict(set)
         number_col = defaultdict(set)
 
         for i in range(len(board)):
             for j in range(len(board[0])):
                 if board[i][j].isnumeric():
-                    location = f"{i//3}{j//3}"
-
-                    if board[i][j] in number_locations[location]:
+                    if board[i][j] in squares[(i//3,j//3)]:
                         return False
                     
                     if i in number_row[board[i][j]] or j in number_col[board[i][j]]:
                         return False
 
-                    number_locations[location].add(board[i][j])
+                    squares[(i//3,j//3)].add(board[i][j])
                     number_row[board[i][j]].add(i)
                     number_col[board[i][j]].add(j)
         return True
