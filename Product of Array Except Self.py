9bc938f29b313c77be876363b5be81099f23ccd8
Saravana-Ace <saravana.polisetti@gmail.com>
2025-06-10 01:56:50 -0700

    Product of Array Except Self

---
+++ Product of Array Except Self.py
@@ -0,0 +1,21 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        forward, backward = [], []
+
+        forward.append(1)
+        temp = 1
+        for num in nums:
+            temp *= num
+            forward.append(temp)
+
+        temp = 1
+        for j in range(len(nums)-1, -1, -1):
+            temp *= nums[j]
+            backward.append(temp)
+        backward.append(1)
+
+        res = []
+        disp = len(backward)-3
+        length = len(forward)-1
+        
+        return [forward[i] * backward[disp-i] for i in range(length)]
