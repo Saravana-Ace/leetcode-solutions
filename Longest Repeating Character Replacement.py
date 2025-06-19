abb7206bb105c6503bcd6a17d28a025e1dc7855b
Saravana-Ace <saravana.polisetti@gmail.com>
2025-06-19 15:54:13 -0700

    Longest Repeating Character Replacement

--- Longest Repeating Character Replacement.py
+++ Longest Repeating Character Replacement.py
@@ -4,27 +4,20 @@ class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         char_count = defaultdict(int)
 
-        # def get_window_max(l,r):
-        #     c = 0
-        #     for i in range(l,r+1):
-        #         c = max(c, char_count[s[i]])
-        #     return c
-
-        l, r = 0, 0
+        l = 0
         res = 0
         curr_window_max_c = 0
+
         for r in range(len(s)):
             char_count[s[r]] += 1
-            window = (r-l+1)
+            # window = (r-l+1)
             
             curr_window_max_c = max(curr_window_max_c, char_count[s[r]])
             
-            if window-curr_window_max_c <= k:
-                res = max(res, window)
-                continue
+            while (r-l+1)-curr_window_max_c > k:
+                char_count[s[l]] -= 1
+                l += 1          
+            
+            res = max(res, r-l+1)
 
-            char_count[s[l]] -= 1
-            l += 1
-
-        return res
-                
+        return res        
