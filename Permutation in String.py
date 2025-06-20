2a40f7226e5e60d0209a07c54591518e0903dbea
Saravana-Ace <saravana.polisetti@gmail.com>
2025-06-19 18:20:36 -0700

    Permutation in String

---
+++ Permutation in String.py
@@ -0,0 +1,27 @@
+from collections import defaultdict, Counter
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        print(len(s2))
+        l = 0
+        r = 0
+        check = Counter(s1)
+        window_char_count = defaultdict(int)
+        unique_char_count = 0
+        for r in range(len(s2)):
+            window_char_count[s2[r]] += 1
+
+            if s2[r] in check and window_char_count[s2[r]] == check[s2[r]]:
+                unique_char_count += 1
+            
+            if unique_char_count == len(check): return True
+
+            if r-l+1 == len(s1):
+
+                if s2[l] in check and window_char_count[s2[l]] == check[s2[l]]:
+                    unique_char_count -= 1
+                
+                window_char_count[s2[l]] -= 1
+                l += 1
+
+        return False
+            
