20c8e5879e6bb412437c9cf70c2e0dbab909ef57
Saravana-Ace <saravana.polisetti@gmail.com>
2025-06-26 03:09:32 -0700

    Sum of k-Mirror Numbers

---
+++ Sum of k-Mirror Numbers.py
@@ -0,0 +1,27 @@
+class Solution:
+    def kMirror(self, k: int, n: int) -> int:
+        def convert_and_check(num):
+            check = []
+            while num != 0:
+                rem = num%k
+                num = num//k
+                check.append(rem)
+            return check == check[::-1]
+        
+        l = 1
+        res = []
+
+        while len(res) < n:
+            r = l * 10
+            for j in [1,0]:
+                for i in range(l, r):
+                    if len(res) == n: break
+                    
+                    copy = str(i)
+                    p = int(copy[:-1] + copy[::-1]) if j == 1 else int(copy + copy[::-1])
+
+                    if convert_and_check(p):
+                        res.append(p)
+            
+            l = r
+        return sum(res)
