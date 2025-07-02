c4cbf393af2163620322bdbc3ac138276e4ec6da
Saravana-Ace <saravana.polisetti@gmail.com>
2025-07-01 18:40:24 -0700

    Find the Original Typed String I

--- Find the Original Typed String I.py
+++ Find the Original Typed String I.py
@@ -1,16 +1,7 @@
 class Solution:
     def possibleStringCount(self, word: str) -> int:
-        count = 1
-        prev_letter = None
-        i = 0
         res = 0
-        for i in range(len(word)):
-            if word[i] == prev_letter:
-                count += 1
-            else:
-                res += count-1
-                count = 1
-            prev_letter = word[i]
-            
-        res += count-1
+        for i in range(1, len(word)):
+            if word[i] == word[i-1]:
+                res += 1
         return res + 1
