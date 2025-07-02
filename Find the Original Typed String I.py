7fe63dd928ebe931c5616ba0d8e8c501a00a6686
Saravana-Ace <saravana.polisetti@gmail.com>
2025-07-01 18:37:05 -0700

    Find the Original Typed String I

--- Find the Original Typed String I.py
+++ Find the Original Typed String I.py
@@ -4,13 +4,13 @@ class Solution:
         prev_letter = None
         i = 0
         res = 0
-        while i < len(word):
+        for i in range(len(word)):
             if word[i] == prev_letter:
                 count += 1
             else:
                 res += count-1
                 count = 1
             prev_letter = word[i]
-            i += 1
+            
         res += count-1
         return res + 1
